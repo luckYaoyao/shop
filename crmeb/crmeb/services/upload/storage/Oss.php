47f1bf25a13ecccafbbeb050ab6a928bec9bb887
evoxwht <442384644@qq.com>
2021-11-18 16:58:48 +0800

    增加缩略图开关

--- crmeb/crmeb/services/upload/storage/Oss.php
+++ crmeb/crmeb/services/upload/storage/Oss.php
@@ -199,7 +199,7 @@ class Oss extends BaseUpload
                     $height = 'thumb_' . $v . '_height';
                     $width = 'thumb_' . $v . '_width';
                     $key = 'filePath' . ucfirst($v);
-                    if (isset($config[$height]) && isset($config[$width]) && $config[$height] && $config[$width]) {
+                    if (sys_config('image_thumbnail_status', 1) && isset($config[$height]) && isset($config[$width]) && $config[$height] && $config[$width]) {
                         $this->fileInfo->$key = $filePath . '?x-oss-process=image/resize,h_' . $config[$height] . ',w_' . $config[$width];
                         $this->fileInfo->$key = $this->water($this->fileInfo->$key);
                         $data[$v] = $this->fileInfo->$key;
