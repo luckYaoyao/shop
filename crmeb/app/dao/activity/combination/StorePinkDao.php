f7387c7369f2775565518e1c50c539d2479d20de
吴昊天 <442384644@qq.com>
2023-03-20 15:53:58 +0800

    search 优化

--- crmeb/app/dao/activity/combination/StorePinkDao.php
+++ crmeb/app/dao/activity/combination/StorePinkDao.php
@@ -54,7 +54,7 @@ class StorePinkDao extends BaseDao
      */
     public function getList(array $where, int $page = 0, int $limit = 0)
     {
-        return $this->search($where)->when($where['k_id'] != 0, function ($query) use ($where) {
+        return $this->search($where, false)->when($where['k_id'] != 0, function ($query) use ($where) {
             $query->whereOr('id', $where['k_id']);
         })->with('getProduct')->when($page != 0, function ($query) use ($page, $limit) {
             $query->page($page, $limit);
