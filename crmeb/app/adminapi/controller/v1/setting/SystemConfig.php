6dfa7545af2cf134aea38e03a93590359632222a
evoxwht <442384644@qq.com>
2023-04-12 14:07:30 +0800

    删除配置保存的时候的无用代码

--- crmeb/app/adminapi/controller/v1/setting/SystemConfig.php
+++ crmeb/app/adminapi/controller/v1/setting/SystemConfig.php
@@ -311,15 +311,6 @@ class SystemConfig extends AuthController
             return app('json')->fail(500029);
         }
 
-        //支付接口类型选择，如果有订单就不能再进行切换
-//        if (isset($post['pay_wechat_type'])) {
-//            /** @var StoreOrderServices $orderServices */
-//            $orderServices = app()->make(StoreOrderServices::class);
-//            if ($post['pay_wechat_type'] != -1 && $orderServices->count()) {
-//                return app('json')->fail('支付接口类型已经选择，不能再次进行切换，切换后会导致无法退款等问题。');
-//            }
-//        }
-
         if (isset($post['weixin_ckeck_file'])) {
             $from = public_path() . $post['weixin_ckeck_file'];
             $to = public_path() . array_reverse(explode('/', $post['weixin_ckeck_file']))[0];
