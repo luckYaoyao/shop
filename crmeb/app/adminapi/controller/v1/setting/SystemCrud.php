68851f311e7b337f33d96681d242f610674a8a9d
liaofei <136327134@qq.com>
2023-04-24 16:59:14 +0800

    修改

--- crmeb/app/adminapi/controller/v1/setting/SystemCrud.php
+++ crmeb/app/adminapi/controller/v1/setting/SystemCrud.php
@@ -254,7 +254,7 @@ class SystemCrud extends AuthController
             $item['default_field_type'] = $item['field_type'];
             $item['default_comment'] = $item['comment'];
             $item['default_default'] = $item['default'];
-            $item['primaryKey'] = (int)$item['primaryKey'];
+            $item['primaryKey'] = isset($item['primaryKey']) ? (int)$item['primaryKey'] : 0;
             $info['field']['tableField'][$key] = $item;
         }
         //对比数据库,是否有新增字段
