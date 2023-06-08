01990a29d5f958146b36df1f14d66fa310bc4d3f
evoxwht <442384644@qq.com>
2023-06-08 18:27:40 +0800

    优惠券领取不减少数量问题修复

--- crmeb/app/services/activity/coupon/StoreCouponIssueServices.php
+++ crmeb/app/services/activity/coupon/StoreCouponIssueServices.php
@@ -374,9 +374,13 @@ class StoreCouponIssueServices extends BaseServices
         $issueUserService = app()->make(StoreCouponIssueUserServices::class);
         /** @var StoreCouponUserServices $couponUserService */
         $couponUserService = app()->make(StoreCouponUserServices::class);
-        $this->transaction(function () use ($issueUserService, $uid, $id, $couponUserService, $issueCouponInfo) {
+        $this->transaction(function () use ($issueUserService, $uid, $id, $couponUserService, $issueCouponInfo, $is_receive) {
             $issueUserService->save(['uid' => $uid, 'issue_coupon_id' => $id, 'add_time' => time()]);
             $couponUserService->addUserCoupon($uid, $issueCouponInfo, "send");
+            if ($issueCouponInfo['total_count'] > 0 && $is_receive) {
+                $issueCouponInfo['remain_count'] -= 1;
+                $issueCouponInfo->save();
+            }
         });
     }
 
