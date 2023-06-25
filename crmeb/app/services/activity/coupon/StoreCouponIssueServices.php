3aed087cf3631dd98a13bfa875503a592ee2bc37
liaofei <136327134@qq.com>
2023-06-25 09:53:41 +0800

    修改优惠卷

--- crmeb/app/services/activity/coupon/StoreCouponIssueServices.php
+++ crmeb/app/services/activity/coupon/StoreCouponIssueServices.php
@@ -65,7 +65,7 @@ class StoreCouponIssueServices extends BaseServices
         foreach ($list as &$item) {
             $item['use_time'] = date('Y-m-d', $item['start_use_time']) . ' ~ ' . date('Y-m-d', $item['end_use_time']);
         }
-        unset($where['type'], $where['receive_type']);
+//        unset($where['type'], $where['receive_type']);
         $count = $this->dao->count($where);
         return compact('list', 'count');
     }
